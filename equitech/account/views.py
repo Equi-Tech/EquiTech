e064a3aac503ca575fa580f7ab14ff180d06cb08
Eshan Roy (Eshanized) <148610067+eshanized@users.noreply.github.com>
2024-05-15 04:08:07 +0530

    perf(fn): add more functionality

--- equitech/account/views.py
+++ equitech/account/views.py
@@ -23,9 +23,46 @@ def account(request): # contains information about the current request from the 
             kyc = KYC.objects.get(user = user)
         # KYC information is not found (an exception is raised), it displays a warning message
         except:
-            messages.warning(request, "KYC Not Found!")
+            messages.warning(request, "User Must Complete KYC!")
             return redirect("accounnt:kyc-form")
     # displays a warning message indicating that KYC information is not found and redirects the user to fill out the KYC form
     else:
-        messages.warning(request, "KYC Not Found!")
-        return redirect("accounnt:kyc-form")
+        messages.warning(request, "Must be logged in!")
+        return redirect("usersauth:sign-in")
+    
+    context = {
+        'account':account,
+        'kyc':kyc,
+    }
+    return render(request, 'account/account.html', context)
+
+
+def kyc_registration(request):
+    user = request.user
+    account = Account.objects.get(user = user)
+    try:
+        kyc = KYC.objects.get(user = user)
+    except:
+        kyc = None
+    
+    if request.method == "POST":
+        form = KYCForm(request.POST, request.FILES, instance=kyc)
+        if form.is_valid():
+            new_form = form.save(commit=False)
+            new_form.user = user
+            new_form.account = account
+            new_form.save()
+            messages.success(request, "KYC Submission Successful.")
+            return redirect("account:dashboard")
+    else:
+        form = KYCForm(instance=kyc)
+    
+    context = {
+        "account": account,
+        "form": form,
+        "kyc":kyc,
+    }
+    return render(request, "account/kyc-form.html", context)
+
+
+def Dashboard(request):
