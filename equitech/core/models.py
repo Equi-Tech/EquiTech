fd1332869938e9d841c2e9aa6172427ff72b824a
Eshan Roy (Eshanized) <148610067+eshanized@users.noreply.github.com>
2024-05-15 04:18:49 +0530

    test(models): changes in models

--- equitech/core/models.py
+++ equitech/core/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from shortuuid.django_fields import ShortUUIDField
+from usersauth.models import User
+from account.models import Account
+
 
 # Create your models here.
+
+TRANSACTION_TYPE = (
+    ("transfer", "Transfer"),
+    ("recieved", "Recieved"),
+    ("withdraw", "withdraw"),
+    ("refund", "Refund"),
+    ("request", "Payment Request"),
+    ("none", "None")
+)
+
+TRANSACTION_STATUS = (
+    ("failed", "failed"),
+    ("completed", "completed"),
+    ("pending", "pending"),
+    ("processing", "processing"),
+    ("request_sent", "request_sent"),
+    ("request_settled", "request settled"),
+    ("request_processing", "request processing"),
+
+)
+
+
+
+CARD_TYPE = (
+    ("master", "master"),
+    ("visa", "visa"),
+    ("rupay", "rupay"),
+
+)
+
+
+NOTIFICATION_TYPE = (
+    ("None", "None"),
+    ("Transfer", "Transfer"),
+    ("Credit Alert", "Credit Alert"),
+    ("Debit Alert", "Debit Alert"),
+    ("Sent Payment Request", "Sent Payment Request"),
+    ("Recieved Payment Request", "Recieved Payment Request"),
+    ("Funded Credit Card", "Funded Credit Card"),
+    ("Withdrew Credit Card Funds", "Withdrew Credit Card Funds"),
+    ("Deleted Credit Card", "Deleted Credit Card"),
+    ("Added Credit Card", "Added Credit Card"),
+
+)
