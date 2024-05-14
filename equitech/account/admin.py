955a00a180c5257cc58dff9706edf09239afd3b5
Eshan Roy (Eshanized) <148610067+eshanized@users.noreply.github.com>
2024-05-15 03:06:43 +0530

    fix(models): KYC fixed

--- equitech/account/admin.py
+++ equitech/account/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from import_export.admin import ImportExportActionModelAdmin
 from .models import Account
+from .models import KYC
 
 # Register your models here.
 
