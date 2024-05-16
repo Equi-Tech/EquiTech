1286ef61a40bcdbb780648fcee1f73f3e9d912e8
Eshan Roy (Eshanized) <148610067+eshanized@users.noreply.github.com>
2024-05-16 21:37:29 +0530

    perf(admin): admin models updated

--- equitech/core/admin.py
+++ equitech/core/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import Transaction
+from .models import CreditCard
+from .models import Notification
+
 
 # Register your models here.
+class TransactionAdmin(admin.ModelAdmin):
+    list_editable = [
+        'amount', 
+        'status', 
+        'transaction_type', 
+        'reciver', 
+        'sender',
+        ]
+    list_display = [
+        'user', 
+        'amount', 
+        'status', 
+        'transaction_type',
+        'reciver', 
+        'sender',
+        ]
+
+class CreditCardAdmin(admin.ModelAdmin):
+    list_editable = [
+        'amount', 
+        'card_type',
+        ]
+    list_display = [
+        'user', 
+        'amount', 
+        'card_type',
+        ]    
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = [
+        'user', 
+        'notification_type', 
+        'amount',
+        'date',
+        ]
+
+admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(CreditCard, CreditCardAdmin)
+admin.site.register(Notification, NotificationAdmin)
