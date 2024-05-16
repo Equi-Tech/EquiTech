7f9b2d08e7f0ef6e8cd66c2f63bbd17d5484275c
Eshan Roy (Eshanized) <148610067+eshanized@users.noreply.github.com>
2024-05-16 22:20:30 +0530

    feat(config): url

---
+++ equitech/account/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import kyc_registration, account,Dashboard
+
+app_name = 'account'
+urlpatterns = [
+    path("",account , name='account'),
+    path("dashboard",Dashboard , name='dashboard'),
+    path("kyc-reg/",kyc_registration, name='kyc-reg'),
+]
