ed48e0c870942e343beace36cb6906e8be53a9f1
Eshan Roy (Eshanized) <148610067+eshanized@users.noreply.github.com>
2024-05-16 22:12:37 +0530

    perf(update): update url configs

--- equitech/core/payment_request.py
+++ equitech/core/payment_request.py
@@ -148,10 +148,49 @@ def settlement_confirmation(request, account_number, transaction_id):
 def settlement_processing(request, account_number, transaction_id):
     account = Account.objects.get(account_number=account_number)
     transaction = Transaction.objects.get(transaction_id = transaction_id)
-
     sender = request.user
     sender_account = request.user.account
-
     if request.method == 'POST':
         pin_number = request.POST.get('pin-number')
-        
+        pin_number = request.POST.get('pin-number')
+        if pin_number == sender_account.pin_number:
+            if sender_account.account_balance >=0 or sender_account.account_balance > transaction.amount:
+                sender_account.account_balance -= transaction.amount
+                sender_account.save()
+                account.account_balance += transaction.amount
+                account.save()
+                transaction.status = "request_settled"
+                transaction.save()
+                messages.success(request, f"settled to {account.user.kyc.full_name} was successfull.")
+                return redirect("core:settlement-completed",account.account_number, transaction.transaction_id)
+            else:
+                messages.warning(request, "Insufficient funds, fund your account and try again.")    
+        else:
+            messages.warning(request, "Incorrect Pin")
+            return redirect("core:settlement-confirmation", account.account_number, transaction.transaction_id)
+    else:
+        messages.warning(request, "Error Occured")
+        return redirect("account:dashboard")
+
+def SettlementCompleted(request ,account_number, transaction_id):
+    try:
+        account = Account.objects.get(account_number=account_number)
+        transaction = Transaction.objects.get(transaction_id=transaction_id)
+    except:
+        messages.warning(request, 'Request does not exists')
+        return redirect("account:account")
+    context = {
+        'account':account,
+        'transaction':transaction,
+        } 
+    return render(request, 'payment_request/settlement-completed.html', context)
+
+def delete_payment_request(request ,account_number, transaction_id):
+    account = Account.objects.get(account_number=account_number)
+    transaction = Transaction.objects.get(transaction_id=transaction_id)
+
+    if request.user == transaction.user:
+        transaction.delete()
+        messages.success(request, "Payment Request Deleted Sucessfully.")
+        return redirect("core:transaction-list")
+    
---
+++ equitech/core/urls.py
@@ -0,0 +1,37 @@
+from django.urls import path
+from .views import index
+from .transfer import search_using_account, AmountTransfer, AmountTransferProcess,TransactionConfirmation,TransferProcess, TransferCompleted
+from .transaction import transaction_list, transaction_detail
+from .payment_request import SearchUserRequest, AmountRequest, AmountRequestProcess, RequestConfirmation, RequestCompleted, RequestFinalProcess, settlement_confirmation, settlement_processing, SettlementCompleted, delete_payment_request
+from .credit_card import credit_card_detail, fund_credit_card, withdraw_credit_card, delete_card
+
+
+app_name = 'core'
+urlpatterns = [
+    path("", index, name='index'),
+    path('search-account/', search_using_account, name='search-account'),
+    path('amount-transfare/<account_number>/',AmountTransfer , name='amount-transfare'),
+    path('amount-transfare-process/<account_number>/',AmountTransferProcess , name='amount-transfare-Process'),
+    path('transfare-confirm/<account_number>/<transaction_id>/',TransactionConfirmation , name='transfare-confirmation'),
+    path('transfare-process/<account_number>/<transaction_id>/',TransferProcess , name='transaction-process'),
+    path('transfare-completed/<account_number>/<transaction_id>/',TransferCompleted , name='transfar-completed'),
+    #transaction
+    path('transaction/',transaction_list, name='transaction-list' ),
+    path('transaction/<transaction_id>',transaction_detail, name='transaction-detail' ),
+    #payment_request
+    path('request-search-user/',SearchUserRequest, name='request-search-user' ),
+    path('amount-request/<account_number>',AmountRequest, name='amount-request' ),
+    path('amount-request-process/<account_number>/',AmountRequestProcess , name='amount-request-Process'),
+    path('request-confirm/<account_number>/<transaction_id>/',RequestConfirmation , name='request-confirmation'),
+    path('request-process/<account_number>/<transaction_id>/',RequestFinalProcess , name='request-finial-process'),
+    path('request-completed/<account_number>/<transaction_id>/',RequestCompleted , name='request-completed'),
+    path('settlement-confirmation/<account_number>/<transaction_id>/', settlement_confirmation, name='settlement-confirmation'),
+    path('settlement-processing/<account_number>/<transaction_id>/',settlement_processing, name= 'settlement_processing'),
+    path('settlement-completed/<account_number>/<transaction_id>/',SettlementCompleted , name='settlement-completed'),
+    path('delete-request/<account_number>/<transaction_id>/',delete_payment_request , name='delete-request'),
+    # credit_card
+    path('card/<card_id>/', credit_card_detail, name="card_detail" ),
+    path('fund-credit-card/<card_id>/',fund_credit_card, name= "fund-credit-card" ),
+    path('withdraw-credit-card/<card_id>/',withdraw_credit_card, name= "withdraw-credit-card" ),
+    path('delete-credit-card/<card_id>/',delete_card, name= "delete-card" ),
+]
