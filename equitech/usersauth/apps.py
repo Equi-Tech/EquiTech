67a767e72db961dbb3b6f08637a8c07f68bb6c01
deep-commits <sharmad8363@gmail.com>
2024-05-20 08:15:28 +0530

    clean all files

--- equitech/usersauth/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsersauthConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'usersauth'
--- equitech/usersauth/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import RegisterView, LoginView, LogoutView
-
-app_name = 'usersauth'
-urlpatterns = [
-    path("sign-up/",RegisterView, name='sign-up'),
-    path("sign-in/",LoginView, name='sign-in'),
-    path("sign-out/",LogoutView, name='sign-out'),
-]
